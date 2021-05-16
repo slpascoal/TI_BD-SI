31abc5f41d5f71f48ee1916ec077dabb23cd20cd
Silas L. Pascoal <of.scode@gmail.com>
2021-05-16 20:57:42 -0300

    Update: Localizações de Pastas e Aplicações
    Desenvolvimento das aplicações java, sql e front-end. Além de criarmos as pastas src para a organização que foi pedida no Sprint 3

---
+++ src/main/java/Usuario/Operacoes.java
@@ -0,0 +1,181 @@
+/*
+/	CLASSES DO GERENCIADOR FINANCEIRO
+/	RECEITA DESPESA TRANSACAO
+*/
+
+import java.util.Random;
+
+public class Operacoes {
+	private int id;
+    private int saldoAtual;
+	Random geradorId = new Random();
+
+	public Operacoes() {
+        this.id = geradorId.nextInt(1000);//FUNÇÃO PARA FAZER O ID
+        this.saldoAtual = -1;
+	}
+	
+	public Operacoes(String saldoAtual) {
+        this.id = geradorId.nextInt(1000);//FUNÇÃO PARA FAZER O ID
+		this.saldoAtual = Integer.parseInt(saldoAtual);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId() {
+	
+		id = geradorId.nextInt(1000);
+	
+    }
+
+	public String getSaldoAtual() {
+		return Integer.toString(saldoAtual);
+	}
+
+	public void setSaldoAtual(String saldoAtual) {
+		this.saldoAtual = Integer.parseInt(saldoAtual);
+	}
+
+	public String toString() {
+		return "Operacoes Dados : [id=" + id + ", Saldo Atual=" + saldoAtual + "]";
+	}
+	
+}
+
+class Receita extends Operacoes {
+	private String resumo;
+	private int valor;
+
+	public Receita() {
+		setId();
+		this.resumo = "";
+		this.valor = -1;
+	}
+
+	public Receita(int id, String resumo, int valor) {
+		this.id = id;
+		this.resumo = resumo;
+		this.valor = valor
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId() {
+		thid.id = id;
+    }
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor() {
+		this.valor = valor;
+    }
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+
+	public String toString() {
+		return "Receita Dados : [valor=" + valor + ", Resumo=" + resumo + "]";
+	}
+}
+
+class Despesa extends Operacoes {
+	private String resumo;
+	private int valor;
+
+	public Despesa() {
+		this.id = -1;
+		this.resumo = "";
+		this.valor = -1;
+	}
+
+	public Despesa(int id, String resumo, int valor) {
+		this.id = id;
+		this.resumo = resumo;
+		this.valor = valor
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		thid.id = id;
+    }
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor() {
+		this.valor = valor;
+    }
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+
+	public String toString() {
+		return "Despesa Dados : [valor=" + valor + ", Resumo=" + resumo + "]";
+	}
+}
+
+
+class Transacoes extends Operacoes {
+	private String resumo;
+	private int valor;
+
+	public Transacoes() {
+		this.id = -1;
+		this.resumo = "";
+		this.valor = -1;
+	}
+
+	public Transacoes(int id, String resumo, int valor) {
+		this.id = id;
+		this.resumo = resumo;
+		this.valor = valor
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		thid.id = id;
+    }
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor() {
+		this.valor = valor;
+    }
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+
+	public String toString() {
+		return "Despesa Dados : [valor=" + valor + ", Resumo=" + resumo + "]";
+	}
+}
---
+++ src/main/resources/Site/README.md
@@ -0,0 +1,4 @@
+# Sites Faculdade
+ Sites e Projetos WEB criados para trabalhos da faculdade.
+ 
+ Criador: Silas Lopes Pascoal
